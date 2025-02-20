a4345ff7b74461c60e6d5f887333b70c8aced4b1
Gautier <gautier.blandin@gmail.com>
2025-02-20 19:26:27 +0100

    infra: add working deployment script

--- apps/infrastructure/index.ts
+++ apps/infrastructure/index.ts
@@ -1,9 +1,78 @@
 import * as pulumi from '@pulumi/pulumi';
 import * as aws from '@pulumi/aws';
-import * as awsx from '@pulumi/awsx';
+import * as apigateway from '@pulumi/aws-apigateway';
 
-// Create an AWS resource (S3 Bucket)
-const bucket = new aws.s3.BucketV2('my-bucket');
+// Get the current stack's config
+const config = new pulumi.Config();
+// Create DynamoDB table
+const timeCapsuleTable = new aws.dynamodb.Table('timeCapsuleTable', {
+  attributes: [
+    { name: 'PK', type: 'S' },
+    { name: 'SK', type: 'S' },
+    { name: 'GSI1PK', type: 'S' },
+    { name: 'GSI1SK', type: 'S' },
+  ],
+  hashKey: 'PK',
+  rangeKey: 'SK',
+  billingMode: 'PAY_PER_REQUEST',
+  globalSecondaryIndexes: [
+    {
+      name: 'GSI1',
+      hashKey: 'GSI1PK',
+      rangeKey: 'GSI1SK',
+      projectionType: 'ALL',
+    },
+  ],
+});
 
-// Export the name of the bucket
-export const bucketName = bucket.id;
+// Create Lambda function
+const lambdaRole = new aws.iam.Role('lambdaRole', {
+  assumeRolePolicy: JSON.stringify({
+    Version: '2012-10-17',
+    Statement: [
+      {
+        Action: 'sts:AssumeRole',
+        Effect: 'Allow',
+        Principal: {
+          Service: 'lambda.amazonaws.com',
+        },
+      },
+    ],
+  }),
+});
+
+new aws.iam.RolePolicyAttachment('lambdaRolePolicy', {
+  role: lambdaRole,
+  policyArn: aws.iam.ManagedPolicy.AWSLambdaBasicExecutionRole,
+});
+
+new aws.iam.RolePolicyAttachment('lambdaDynamoDBPolicy', {
+  role: lambdaRole,
+  policyArn: aws.iam.ManagedPolicy.AmazonDynamoDBFullAccess,
+});
+
+const lambda = new aws.lambda.Function('timeCapsuleLambda', {
+  code: new pulumi.asset.FileArchive('../../dist/apps/server'),
+  handler: 'main.handler',
+  role: lambdaRole.arn,
+  runtime: 'nodejs20.x',
+  environment: {
+    variables: {
+      TIME_CAPSULE_TABLE: timeCapsuleTable.name,
+    },
+  },
+});
+
+// Create API Gateway
+const api = new apigateway.RestAPI('api', {
+  routes: [
+    {
+      path: '/timecapsule',
+      method: 'POST',
+      eventHandler: lambda,
+    },
+  ],
+});
+
+// Export the API endpoint
+export const apiUrl = api.url;
--- docs/manual-steps.md
+++ docs/manual-steps.md
@@ -1,5 +1,7 @@
 This is a temporary document. The manual steps described here should be automated at part of the deployment process.
 
+# Server
+
 Build the server app:
 ```bash
 nx build server
@@ -9,4 +11,13 @@ Install dependencies of the server app after building it:
 ```bash
 cd dist/apps/server
 npm i --only=production
+```
+
+# Infrastructure
+
+Set AWS credentials:
+
+```bash
+export AWS_ACCESS_KEY_ID=XXXXXXXXXXXXXXXXXXXX
+export AWS_SECRET_ACCESS_KEY=XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
 ```
