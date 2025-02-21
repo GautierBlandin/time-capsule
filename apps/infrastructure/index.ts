20aa02f964a1d492bd9360aa3cc20710e9d11897
Gautier <gautier.blandin@gmail.com>
2025-02-21 22:43:47 +0100

    infra: add event bridge scheduled rule and send time capsules lambda function

--- apps/infrastructure/index.ts
+++ apps/infrastructure/index.ts
@@ -1,4 +1,5 @@
 import * as pulumi from '@pulumi/pulumi';
+import * as aws from '@pulumi/aws';
 import {
   createTimeCapsuleDynamoDBTable,
   createTimeCapsuleLambda,
@@ -24,3 +25,41 @@ const { distribution } = createTimeCapsuleCloudFrontDistribution({
 });
 
 export const cloudFrontUrl = distribution.domainName;
+
+// Create the lambda function for sending time capsules
+const sendTimeCapsuleLambda = new aws.lambda.Function('sendTimeCapsuleLambda', {
+  code: new pulumi.asset.FileArchive('../../dist/apps/server/sendTimeCapsules'),
+  handler: 'sendTimeCapsules.handler',
+  role: lambda.role, // Reuse the role from the existing lambda
+  runtime: 'nodejs20.x',
+  environment: {
+    variables: {
+      TIME_CAPSULE_TABLE: timeCapsuleTable.name,
+    },
+  },
+});
+
+// Create a scheduled rule to trigger the lambda every minute
+const scheduledRule = new aws.cloudwatch.EventRule(
+  'sendTimeCapsuleScheduledRule',
+  {
+    scheduleExpression: 'rate(1 minute)',
+  }
+);
+
+// Grant the scheduled rule permission to invoke the Lambda function
+const lambdaPermission = new aws.lambda.Permission(
+  'scheduledRuleLambdaPermission',
+  {
+    action: 'lambda:InvokeFunction',
+    function: sendTimeCapsuleLambda,
+    principal: 'events.amazonaws.com',
+    sourceArn: scheduledRule.arn,
+  }
+);
+
+// Connect the scheduled rule to the lambda function
+const ruleTarget = new aws.cloudwatch.EventTarget('sendTimeCapsuleRuleTarget', {
+  rule: scheduledRule.name,
+  arn: sendTimeCapsuleLambda.arn,
+});
