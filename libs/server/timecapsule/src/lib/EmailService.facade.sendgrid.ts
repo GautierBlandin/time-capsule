875ecb6a3c7d994e266cf15106d8b9bf2143f86d
Gautier <gautier.blandin@gmail.com>
2025-02-22 11:10:49 +0100

    feat: remove constructor setting of sendgrid api key

--- libs/server/timecapsule/src/lib/EmailService.facade.sendgrid.ts
+++ libs/server/timecapsule/src/lib/EmailService.facade.sendgrid.ts
@@ -4,14 +4,6 @@ import * as sgMail from '@sendgrid/mail';
 export class SendGridEmailService implements EmailServiceFacade {
   private readonly sender = 'noreply@timecapsules.gautierblandin.com';
 
-  constructor() {
-    const apiKey = process.env['SENDGRID_API_KEY'];
-    if (!apiKey) {
-      throw new Error('SENDGRID_API_KEY environment variable is not set');
-    }
-    sgMail.setApiKey(apiKey);
-  }
-
   async sendEmail({
     to,
     subject,
